9b6afc7895690dae5dfc91013fa0fabd62dcbf65
Mahboob-A <iammahboob.a@gmail.com>
2024-04-02 11:36:11 +0530

    add: core_apps.common.model.TimeStampModel
    TimeStampModel for default ID and time realted abstract class

--- src/core_apps/common/models.py
+++ src/core_apps/common/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+import uuid 
+
+
+class TimeStampModel(models.Model): 
+    ''' Default abstract model for id and time '''
+    pkid = models.BigAutoField(primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta: 
+        abstract = True 
+        ordering = ['-created_at', '-updated_at']
+    
