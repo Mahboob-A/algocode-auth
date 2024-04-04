088f47b5275ad9f73688733207665721d01b9b30
Mahboob-A <iammahboob.a@gmail.com>
2024-04-04 14:48:49 +0530

    feat: core_apps.users.views.CustomUserDetailsView
    followed RESTful API convention for the User-Details API.
    
    add renderers in the api and links for the self user id and associated profile

--- src/core_apps/common/utils.py
+++ src/core_apps/common/utils.py
@@ -4,14 +4,27 @@ def get_current_host(request) -> str:
     return f"{scheme}://{request.get_host()}"
 
 
-def generate_full_url(request, instance, instance_user=False) -> str:
-    ''' Returns the full instance url '''
-    
-    if instance_user:  # url for instance.user one to one relationship (user instance url)
+def generate_full_url(
+    request, instance, instance_user=False, instance_profile=None
+) -> str:
+    """Returns the full instance url
+    instance_user => profile.user
+    instance_profile => user.profile
+
+    if we want the User url if instance=Profile, pass instance_user
+    if we want the Profile url if instance=User, pass instance_profile
+    """
+    if (
+        instance_user
+    ):  # instance is Profile. (url for instance.user one to one relationship (user instance url))
         host = get_current_host(request=request)
-        instance_url = instance.user.get_absolute_url()  # defined in model 
-        return host + instance_url 
-    else: # general instance url 
+        instance_url = instance.user.get_absolute_url()  # defined in model
+        return host + instance_url
+    elif instance_profile:  # instance is User
+        host = get_current_host(request=request)
+        instance_url = instance.profile.get_absolute_url()
+        return host + instance_url
+    else:  # general instance url
         host = get_current_host(request=request)
         instance_url = instance.get_absolute_url()
-        return host + instance_url 
+        return host + instance_url
