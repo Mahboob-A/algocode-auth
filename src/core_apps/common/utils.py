7ffc0c5db2dcf3a3d96e511244cea39c0e74b3f3
Mahboob-A <iammahboob.a@gmail.com>
2024-06-06 22:02:08 +0530

    fix: uheatoas constuction for dev and prodution stage

--- src/core_apps/common/utils.py
+++ src/core_apps/common/utils.py
@@ -1,9 +1,27 @@
+from urllib.parse import urlparse
+
 def get_current_host(request) -> str:
     """Returns the host url with current scheme"""
-    scheme = request.is_secure() and "https" or "http"
-    port = 8000
-    # http://127.0.0.1:8000
-    return f"{scheme}://{request.get_host()}:{port}"
+
+    scheme = "https" if request.is_secure() else "http"
+    host = request.get_host()
+
+    parsed_host = urlparse(f"{scheme}://{host}")
+    hostname = parsed_host.hostname
+
+    if hostname in ["127.0.0.1", "localhost"]:
+        port = 8000
+        return f"{scheme}://{hostname}:{port}"
+    else:
+        return f"{scheme}://{hostname}"
+
+    # scheme = request.is_secure() and "https" or "http"
+    # if host is localhost, add 8000 as port. else do not add any port.
+    # if request.get_host() == "127.0.0.1": 
+    #     port = 8000
+    #     return f"{scheme}://{request.get_host()}:{port}"
+    # else: 
+    #     return f"{scheme}://{request.get_host()}"
 
 
 def generate_full_url(
